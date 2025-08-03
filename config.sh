c56a824f6a881315a4c43dcde9a9b7ddef55ced0
shmily2724 <shmily2724@gmail.com>
2025-08-04 01:01:18 +0700

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,9 @@
+
+PROJECT_DIR=~/BRCA_project
+RAW_DATA_DIR=${PROJECT_DIR}/raw_data
+TRIMMED_DIR=${PROJECT_DIR}/trimmed_data
+ALIGNMENT_DIR=${PROJECT_DIR}/alignments
+VARIANTS_DIR=${PROJECT_DIR}/variants
+QC_DIR=${PROJECT_DIR}/qc_reports
+REFERENCE=~/reference/reference.fa
+THREADS=8
