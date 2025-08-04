7d59c3f7a36b90adfd940c7cdcc0c8b2fab5abaf
shmily2724 <shmily2724@gmail.com>
2025-08-05 00:44:30 +0700

    Update bwa.sh

--- bwa.sh
+++ bwa.sh
@@ -1,16 +1,19 @@
 SAMPLE_NAME=$1
+#tạo các biến đường dẫn
 PROJECT_DIR="/media/shmily/writable/BRCA_project"
-SAMPLE_DIR="${PROJECT_DIR}/${SAMPLE}"
-
+SAMPLE_DIR="${PROJECT_DIR}/results/${SAMPLE_NAME}"
+TRIM_DIR="${SAMPLE_DIR}/trimmed_data"
+BWA_DIR="${SAMPLE_DIR}/Bwa alignments"
+REF="/media/shmily/writable/BRCA_project/reference/reference.fa"
 # File input
-READ1="${RAW_DIR}/${SAMPLE}_1.fastq.gz"
-READ2="${RAW_DIR}/${SAMPLE}_2.fastq.gz"
-
+TRIMMED_R1="${TRIM_DIR}/${SAMPLE_NAME}_1_paired.fastq.gz"
+TRIMMED_R2="${TRIM_DIR}/${SAMPLE_NAME}_2_paired.fastq.gz"
 # File output
-TRIMMED_R1="${TRIM_DIR}/${SAMPLE}_1_paired.fastq.gz"
-UNPAIRED_R1="${TRIM_DIR}/${SAMPLE}_1_unpaired.fastq.gz"
-TRIMMED_R2="${TRIM_DIR}/${SAMPLE}_2_paired.fastq.gz"
-UNPAIRED_R2="${TRIM_DIR}/${SAMPLE}_2_unpaired.fastq.gz"
+BAM="${BWA_DIR}/${SAMPLE_NAME}_aligned.bam"
+mkdir-p "$BWA_DIR"
+# Định nghĩa thông tin Read Group
+# Các biến ${SAMPLE_NAME}, ${THREADS}, ${REF}, ${TRIMMED_R1}, ${TRIMMED_R2}, ${OUTDIR} 
+# cần được định nghĩa trước đó trong script của bạn.
 RG_ID="${SAMPLE_NAME}_RG"
 PLATFORM="Illumina" # Hoặc thay thế bằng nền tảng giải trình tự thực tế của bạn (ví dụ: PacBio, OxfordNanopore)
 LIBRARY_ID="Lib1" # Thay đổi nếu bạn có nhiều thư viện cho cùng một mẫu
@@ -20,8 +23,8 @@ conda activate BRCA
 bwa mem -Y \
   -K 100000000 \
   -t 16 \
-  -R "$rg_string" \
-  "$reference_fasta_file" \
-  "$fastq_file1" \
-  "$fastq_file2" | \
-samtools view -Shb -o "$bam_file" -
+  -R "@RG\tID:${RG_ID}\tSM:${SAMPLE_NAME}\tPL:${PLATFORM}\tLB:${LIBRARY_ID}\tPU:${PLATFORM_UNIT}" \
+  "$REF" \
+  "${TRIMMED_R1}" \
+  "${TRIMMED_R1}" | \
+samtools view -Shb -o "${BAM}" -
