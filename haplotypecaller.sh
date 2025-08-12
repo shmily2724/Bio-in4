823533f78a3d8897f78cb2edd966d702ca59da22
shmily2724 <shmily2724@gmail.com>
2025-08-12 17:00:32 +0700

    Update haplotypecaller.sh

--- haplotypecaller.sh
+++ haplotypecaller.sh
@@ -19,12 +19,9 @@ TARGET_BED="${PROJECT_DIR}/reference/TruSight_Cancer_TargetedRegions_v1.0.hg38.b
 SAMPLE_DIR="${PROJECT_DIR}/results/${SAMPLE_NAME}"
 RECAL_DIR="${SAMPLE_DIR}/recal"
 HAPLO_DIR="${SAMPLE_DIR}/haplotypecaller"
-ANN_DIR="${SAMPLE_DIR}/snpeff"
-SNPEFF_STATS="${ANN_DIR}/${SAMPLE_NAME}" 
 
 # Tạo các thư mục output
 mkdir -p "${HAPLO_DIR}"
-mkdir -p "${ANN_DIR}"
 
 # File input
 RECAL_BAM="${RECAL_DIR}/${SAMPLE_NAME}_recalibrated.bam"
@@ -32,12 +29,6 @@ RECAL_BAM="${RECAL_DIR}/${SAMPLE_NAME}_recalibrated.bam"
 # File output
 HAPLO_GVCF="${HAPLO_DIR}/${SAMPLE_NAME}_gatk.g.vcf.gz"
 HAPLO_VCF="${HAPLO_DIR}/${SAMPLE_NAME}_gatk.vcf.gz"
-ANN_VCF="${ANN_DIR}/${SAMPLE_NAME}_gatk_annotated.vcf"
-
-# Các biến cho SnpEff
-SNPEFF_JAR="/home/shmily/miniconda/envs/GATK/share/snpeff-5.2-1/snpEff.jar"
-SNPEFF_CONFIG="/home/shmily/miniconda/envs/GATK/share/snpeff-5.2-1/snpEff.config"
-SNPEFF_DB="GRCh38.86"
 
 # Kích hoạt môi trường Conda (chỉ một lần)
 eval "$(conda shell.bash hook)"
@@ -69,22 +60,5 @@ echo "GenotypeGVCFs hoàn tất."
 # --- BƯỚC 3: ĐÁNH INDEX CHO FILE VCF ---
 echo "Đánh index cho file VCF..."
 tabix -f -p vcf "${HAPLO_VCF}" # -f để overwrite tránh dừng script tại đây khi chạy lại
-echo "Index hoàn tất."
-
-# --- BƯỚC 4: CHÚ GIẢI VỚI SnpEff ---
-echo "Bắt đầu chú giải với SnpEff..."
-
-# Kiểm tra xem database của SnpEff đã tồn tại chưa
-SNPEFF_DATA_DIR=$(dirname "$SNPEFF_JAR")/data
-if [ ! -d "${SNPEFF_DATA_DIR}/${SNPEFF_DB}" ]; then
-    echo "Thông báo: Database ${SNPEFF_DB} của SnpEff chưa tồn tại. Bắt đầu tải về..."
-    java -jar "$SNPEFF_JAR" download "$SNPEFF_DB" -c "$SNPEFF_CONFIG"
-fi
-
-# SỬA LỖI: Xóa dấu \ ở cuối lệnh
-java -Xmx6g -jar "$SNPEFF_JAR" ann -c "$SNPEFF_CONFIG" -v "$SNPEFF_DB" \
-    -stats "${SNPEFF_STATS}" \
-    "${HAPLO_VCF}" > "${ANN_VCF}"
-
-echo "Pipeline hoàn tất cho mẫu: ${SAMPLE_NAME}!"
+echo "Index hoàn tất cho mẫu: ${SAMPLE_NAME}!"
 
