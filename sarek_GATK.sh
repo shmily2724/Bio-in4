147617d55c7bb34c213dd1a3b800782f32d42665
shmily2724 <shmily2724@gmail.com>
2025-09-18 00:46:51 +0700

    Update sarek_GATK.sh

--- sarek_GATK.sh
+++ sarek_GATK.sh
@@ -25,3 +25,31 @@ nextflow run nf-core/sarek -r 3.5.1 \
   -process.maxCpus 4 \
   -process.maxTime '48 h' \
   -resume
+
+#!/usr/bin/env bash
+set -euo pipefail
+
+# === config ===
+PROJECT_DIR="/home/shmily/BRCA_project"
+BED="$PROJECT_DIR/reference/TruSight_Cancer_TargetedRegions_v1.0.hg38.bed"
+GENOME="GATK.GRCh38"
+INPUT="$PROJECT_DIR/samplesheet_germline.nostatus.csv"          # sample=...,status=normal
+OUTDIR="$PROJECT_DIR/sarek_results/germline_hc"
+
+# Thu gọn heap của Nextflow (tùy chọn)
+export NXF_OPTS='-Xms512m -Xmx1500m'
+
+nextflow run nf-core/sarek -r 3.5.1 \
+  -profile docker \
+  --custom_config_base false \
+  --input "$INPUT" \
+  --outdir "$OUTDIR" \
+  --genome "$GENOME" \
+  --wes \
+  --intervals "$BED" \
+  -c sarek_lowres.config \
+  --tools haplotypecaller \
+  -process.maxMemory '8 GB' \
+  -process.maxCpus 4 \
+  -process.maxTime '48 h' \
+  -resume
